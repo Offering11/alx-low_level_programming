02e586f7225eeeeca1b323344ff2a0c15ed1aa1b
Offering11 <lawaloffering@gmail.com>
2022-10-18 09:14:21 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include<main.h>
+
+/*
+*
+*main - check the code
+*
+*
+*Return: Always 0.
+*/
+
+
+
+int main(void)
+
+{
+
+char ch;
+
+	for(ch='a'; ch <= 'z'; ch++)		
+	putchar(ch);
+	putchar("\n");
+	return (0);
+
+}
