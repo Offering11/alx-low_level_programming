30ed80e24bf74c13715471083aaf190aada40ba5
Offering11 <lawaloffering@gmail.com>
2022-10-18 05:56:55 -0700

    print-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/*
+*Main-Entry point
+*
+*Description : putchar to print -putchar
+*
+*Return : Return 0
+*/
+int main (void)
+
+{
+
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+	return 0;
+}
+
