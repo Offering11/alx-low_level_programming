ea61d88dcb8a430ebf77c4b2844b3c7673dbe989
Offering11 <lawaloffering@gmail.com>
2022-10-20 16:05:49 -0700

    more on nested loop

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isdigit - is a function that check for a digit
+* @c : is the value for c
+* Return: 1 if c is a digit and 0 otherwise
+*/
+
+int _isdigit(int c);
+
+{
+	if ((c >= 0) && (c <= 9))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - is a function that check for uppercase character
+*
+* Return : Successful will return 0
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		_putchar(a);
+	_putchar('/n');
+	return (0);
+}
