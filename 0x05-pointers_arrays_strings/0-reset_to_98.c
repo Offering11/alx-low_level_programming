a1caf7869f8c0a1115ccff4aca27e8dc97572bac
Offering11 <lawaloffering@gmail.com>
2022-10-24 14:23:19 -0700

    Taak on array and pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* reset_to_98 - The prototype function
+* @p: The pointer variable
+* @n: The integar to be print
+*
+* Return: Always 0
+*
+*/
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}
