fd1fe96a90890a06abe3c0f51330c28a131c87ac
root <Offering11>
2023-05-31 16:17:39 -0700

    done

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,9 @@
+document.readyState(function () {
+	  $('DIV#red_header').click(function () {
+		      $('header'.css('color', '#FF0000'));
+		    });
+});document.readyState(function () {
+	  $('DIV#red_header').click(function () {
+		      $('header'.css('color', '#FF0000'));
+		    });
+});
